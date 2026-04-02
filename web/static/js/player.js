d1f007998f18f3e1a863aec49c9adbe69589ae58
MathiasDPX <mathias@dupeux.net>
2026-04-03 00:55:04 +0200

    feat: add mediaSession
    still need some fixes as artworks don't work correctly

--- web/static/js/player.js
+++ web/static/js/player.js
@@ -21,4 +21,62 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     }
+
+    const videoTitle = document.getElementById('video-title').innerText;
+    const channelName = document.getElementById('channel-name').innerText;
+    const channelAvatarURL = document.getElementById('channel-avatar').src;
+
+    const videoId = video['data-video-id']
+    const channelId = video['data-channel-id']
+
+    if ('mediaSession' in navigator) {
+        metadata = {
+            title: videoTitle,
+            artist: channelName,
+            artwork: [
+                {
+                    src: channelAvatarURL,
+                    sizes: "512x512" // lie
+                },
+                {
+                    src: `/data/media/channels/${channelId}/${videoId}/video.webp`,
+                    sizes: "1920x1080" // lie again
+                }
+            ]
+        }
+        console.log("Starting mediaSession with", metadata);
+        navigator.mediaSession.metadata = new MediaMetadata(metadata);
+
+        // player to mediaSession events
+        video.addEventListener('play', function () {
+            navigator.mediaSession.playbackState = "playing";
+        })
+
+        video.addEventListener('pause', function () {
+            navigator.mediaSession.playbackState = "paused";
+        })
+
+        video.addEventListener('ended', function () {
+            navigator.mediaSession.playbackState = "none";
+        })
+
+        // mediaSession to player events
+        navigator.mediaSession.setActionHandler("play", () => {
+            video.play();
+        });
+
+        navigator.mediaSession.setActionHandler("pause", () => {
+            video.pause();
+        });
+
+        setInterval(() => {
+            navigator.mediaSession.setPositionState({
+                duration: video.duration,
+                playbackRate: video.playbackRate,
+                position: video.currentTime
+            })
+        }, 1000);
+    } else {
+        console.warn("mediaSession unavailable on this browser :'(")
+    }
 });
